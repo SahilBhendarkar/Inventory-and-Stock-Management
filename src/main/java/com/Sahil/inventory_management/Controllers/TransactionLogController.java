beab1eac194182848a6f49bb06faa3a724ecec91
SahilBhendarkar <sahilbhendarkar19@gmail.com>
2025-11-19 20:31:28 +0530

    CORS error

--- src/main/java/com/Sahil/inventory_management/Controllers/TransactionLogController.java
+++ src/main/java/com/Sahil/inventory_management/Controllers/TransactionLogController.java
@@ -4,15 +4,13 @@ import com.Sahil.inventory_management.Repository.TransactionLogRepository;
 import com.Sahil.inventory_management.model.TransactionLog;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/transactions")
+@CrossOrigin(origins = "*")
 public class TransactionLogController {
 
     private final TransactionLogRepository transactionLogRepository;
--- src/main/java/com/Sahil/inventory_management/Controllers/UserController.java
+++ src/main/java/com/Sahil/inventory_management/Controllers/UserController.java
@@ -6,13 +6,11 @@ import com.Sahil.inventory_management.DTO.RegisterRequest;
 import com.Sahil.inventory_management.Service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
